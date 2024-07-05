0f45d11168521ca2058d428faeeb79f520527643
Kennyhunter254 <kipronokevin566@gmail.com>
2024-07-05 16:51:07 +0300

    Dom modification

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+//my code
+const main = document.querySelector('main#main');
+main.remove ();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion'; 
+
+
+document.body.appendChild(newHeader);
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { newHeader };
+}
